61222e546d319349163cc848f8a6ee620edb7b71
KishorShresthaGithub <kishorshrestha625@gmail.com>
2021-08-08 12:44:07 +0545

    Removed port from url

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,5 +1,6 @@
-const url = `${process.env.HOST}${
-  process.env.PORT ? `:${process.env.PORT}` : ":80"
-}`;
+// const url = `${process.env.HOST}${
+//   process.env.PORT ? `:${process.env.PORT}` : ":80"
+// }`;
+const url = `${process.env.HOST}`;
 
 export default url;
